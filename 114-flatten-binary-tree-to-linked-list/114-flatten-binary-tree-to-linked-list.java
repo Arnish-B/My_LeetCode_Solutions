d86c30d5d4d8e70bbd22d5c5f18e8d8218a3ac11
Arnish <76063967+Arnish-B@users.noreply.github.com>
2022-08-20 23:24:54 +0530

    Time: 1 ms (80.23%), Space: 42.6 MB (59.35%) - LeetHub

---
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode node) {
+         if (node == null)
+            return;
+        // Or if it is a leaf node
+        if (node.left == null && node.right == null)
+            return;
+        // If root.left children exists then we have to make
+        // it node.right (where node is root)
+        if (node.left != null) {
+            // Move left recursively
+            flatten(node.left);
+            // Store the node.right in Node named tempNode
+            TreeNode tempNode = node.right;
+            node.right = node.left;
+            node.left = null;
+            // Find the position to insert the stored value
+            TreeNode curr = node.right;
+            while (curr.right != null)
+                curr = curr.right;
+            // Insert the stored value
+            curr.right = tempNode;
+        }
+        // Now call the same function for node.right
+        flatten(node.right);
+        
+    }
+}
