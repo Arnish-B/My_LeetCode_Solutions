1075c6ac1125ec00965d7e9b010cc1261618b1bc
Arnish <76063967+Arnish-B@users.noreply.github.com>
2022-12-13 14:23:22 +0530

    Time: 1758 ms (5.01%), Space: 435.8 MB (5.01%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -1,21 +1,31 @@
 class Solution {
     public String reverseWords(String s) {
-        char temp;
-        int last;
-        String str[] = s.split(" ");
-        for(int i = 0;i<str.length;i++){
-            last = str[i].length() - 1;
-            StringBuffer st = new StringBuffer();
-            st.append(str[i]);
-            st.reverse();
-            str[i] = st.toString();
+        // char temp;
+        // int last;
+        // String str[] = s.split(" ");
+        // for(int i = 0;i<str.length;i++){
+        //     last = str[i].length() - 1;
+        //     StringBuffer st = new StringBuffer();
+        //     st.append(str[i]);
+        //     st.reverse();
+        //     str[i] = st.toString();
+        // }
+        // String fin = "";
+        // for(int i = 0;i<str.length;i++){
+        //     for(int j =0;j<str[i].length();j++){
+        //         fin += str[i].charAt(j);
+        //     }
+        //     fin += " ";
+        // }
+        // return fin.trim();
+        String fin = "",temp="";
+        for(int i =s.length()-1;i>=0;i--){
+            temp += s.charAt(i);
         }
-        String fin = "";
-        for(int i = 0;i<str.length;i++){
-            for(int j =0;j<str[i].length();j++){
-                fin += str[i].charAt(j);
-            }
-            fin += " ";
+        System.out.println(temp);
+        String arr[] = temp.split(" ");
+        for(int i = arr.length-1;i>=0;i--){
+            fin += arr[i] + " ";
         }
         return fin.trim();
     }
