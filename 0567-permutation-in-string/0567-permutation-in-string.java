8c313cba1e55c97d4da2af77690ff945f2e155c5
Arnish <76063967+Arnish-B@users.noreply.github.com>
2022-12-16 13:31:21 +0530

    Time: 5 ms (93.06%), Space: 42 MB (96.22%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+         if (s1 == null || s2 == null) return false;
+        if (s2.length() < s1.length()) return false;
+
+        int[] table = new int[26];
+        int[] table2 = new int[26];
+
+        int s1Length = s1.length();
+        int s2Length = s2.length();
+
+        for (int i = 0; i < s1Length; i++) {
+            table[s1.charAt(i) - 'a']++;
+            table2[s2.charAt(i) - 'a']++;
+        }
+
+        for (int i = s1Length; i < s2Length; i++) {
+            if (Arrays.equals(table, table2)) {
+                return true;
+            }
+            table2[s2.charAt(i - s1Length) - 'a']--;
+            table2[s2.charAt(i) - 'a']++;
+        }
+
+        return Arrays.equals(table, table2);
+        
+    }
+}
