123046404118c5a18ad11faae9a922d485be5725
Arnish <76063967+Arnish-B@users.noreply.github.com>
2023-01-11 14:38:45 +0530

    Time: 69 ms (51.76%), Space: 81.5 MB (97.06%) - LeetHub

---
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        System.out.println(adj);
+        for(int [] edge : edges){
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+
+        }
+        System.out.println(adj);
+        return minTimeToCollectApples(0, adj, hasApple, 0);
+        // return 8;
+    }
+    
+    public int minTimeToCollectApples(int index, List<List<Integer>> adj, List<Boolean> hasApple, int parent){
+        int total = 0;
+
+        for(int nbr: adj.get(index)){
+            if(nbr == parent){
+                continue;
+            }
+            total += minTimeToCollectApples(nbr, adj, hasApple, index);
+        }
+        if( index != 0 && (hasApple.get(index) || total > 0))
+          total += 2;
+        return total;
+    }
+}
