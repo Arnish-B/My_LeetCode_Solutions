e6a0fd5cd680e1f3e89b250507e7c202c7d2f5d6
Arnish <76063967+Arnish-B@users.noreply.github.com>
2023-03-28 20:11:08 +0530

    Time: 1 ms (100.00%), Space: 42.3 MB (26.99%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,64 +1,101 @@
+// class Solution {
+    
+//     static class Position {
+//         int x;
+//         int y;
+//         Position(int x, int y) {
+//             this.x = x;
+//             this.y = y;
+//         }
+//     }
+	
+//     public int orangesRotting(int[][] grid) {
+//         Queue<Position> q = new LinkedList<>();
+//         int total = 0, rotten = 0, time = 0;
+		
+		
+//         for (int i = 0; i < grid.length; i++) {
+//             for (int j = 0; j < grid[0].length; j++) {
+//                 if (grid[i][j] == 1 || grid[i][j] == 2) total++;
+//                 if (grid[i][j] == 2) q.offer(new Position(i, j));
+//             }
+//         }
+		
+		
+//         if (total == 0) return 0;
+		
+//         while (! q.isEmpty() && rotten < total) {
+			
+//             int size = q.size();
+			
+			
+//             rotten += size;
+//             if (rotten == total) return time;
+			
+			
+//             time++;
+			
+			
+//             for (int i = 0; i < size; i++) {
+//                 Position p = q.peek();
+				
+				
+//                 if (p.x + 1 < grid.length && grid[p.x + 1][p.y] == 1) {
+//                     grid[p.x + 1][p.y] = 2;
+//                     q.offer(new Position(p.x + 1, p.y));
+//                 }
+//                 if (p.x - 1 >= 0 && grid[p.x - 1][p.y] == 1) {
+//                     grid[p.x - 1][p.y] = 2;
+//                     q.offer(new Position(p.x - 1, p.y));
+//                 }
+//                 if (p.y + 1 < grid[0].length && grid[p.x][p.y + 1] == 1) {
+//                     grid[p.x][p.y + 1] = 2;
+//                     q.offer(new Position(p.x, p.y + 1));
+//                 }
+//                 if (p.y - 1 >= 0 && grid[p.x][p.y - 1] == 1) {
+//                     grid[p.x][p.y - 1] = 2;
+//                     q.offer(new Position(p.x, p.y - 1));
+//                 }
+//                 q.poll();
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 class Solution {
     
-    static class Position {
-        int x;
-        int y;
-        Position(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-	
     public int orangesRotting(int[][] grid) {
-        Queue<Position> q = new LinkedList<>();
-        int total = 0, rotten = 0, time = 0;
-		
-		
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 1 || grid[i][j] == 2) total++;
-                if (grid[i][j] == 2) q.offer(new Position(i, j));
+        if(grid == null || grid.length == 0) return -1;
+        
+        for(int i=0; i<grid.length; i++) {
+            for(int j=0; j<grid[0].length; j++) {
+                if(grid[i][j] == 2) rotAdjacent(grid, i, j, 2);
             }
         }
-		
-		
-        if (total == 0) return 0;
-		
-        while (! q.isEmpty() && rotten < total) {
-			
-            int size = q.size();
-			
-			
-            rotten += size;
-            if (rotten == total) return time;
-			
-			
-            time++;
-			
-			
-            for (int i = 0; i < size; i++) {
-                Position p = q.peek();
-				
-				
-                if (p.x + 1 < grid.length && grid[p.x + 1][p.y] == 1) {
-                    grid[p.x + 1][p.y] = 2;
-                    q.offer(new Position(p.x + 1, p.y));
-                }
-                if (p.x - 1 >= 0 && grid[p.x - 1][p.y] == 1) {
-                    grid[p.x - 1][p.y] = 2;
-                    q.offer(new Position(p.x - 1, p.y));
-                }
-                if (p.y + 1 < grid[0].length && grid[p.x][p.y + 1] == 1) {
-                    grid[p.x][p.y + 1] = 2;
-                    q.offer(new Position(p.x, p.y + 1));
-                }
-                if (p.y - 1 >= 0 && grid[p.x][p.y - 1] == 1) {
-                    grid[p.x][p.y - 1] = 2;
-                    q.offer(new Position(p.x, p.y - 1));
-                }
-                q.poll();
+        
+        int minutes = 2;
+        for(int[] row : grid) {
+            for(int cell : row) {
+                if(cell == 1) return -1;
+                minutes = Math.max(minutes, cell);
             }
         }
-        return -1;
+        
+        return minutes - 2;
+    }
+    
+    private void rotAdjacent(int[][] grid, int i, int j, int minutes) {
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0 || (1 < grid[i][j] && grid[i][j] < minutes)) return;
+        else {
+            grid[i][j] = minutes;
+            rotAdjacent(grid, i - 1, j, minutes + 1);
+            rotAdjacent(grid, i + 1, j, minutes + 1);
+            rotAdjacent(grid, i, j - 1, minutes + 1);
+            rotAdjacent(grid, i, j + 1, minutes + 1);
+        }
     }
 }
